ec08659f04a317f34e299642ed3fed91dba92aa7
Coldweaver <48984157+fleshbones7@users.noreply.github.com>
2022-10-06 20:53:27 +0700

    Create operasi_dasar.py

---
+++ operasi_dasar.py
@@ -0,0 +1,34 @@
+a = 18
+b = 9
+hasil1 = a+b
+print(a,' + ',b,'=',hasil1)  #Penambahan
+#
+c = 72
+d = 4
+hasil2 = c-d
+print(c,'+',d,'=',hasil2)   #Pengurangan
+#
+e = 40
+f = 3
+hasil3 = e*f
+print(e,'*',f,'=',hasil3)   #Perkalian
+#
+g = 63
+h = 8
+hasil4 = g/h
+print(g,'/',h,'=',hasil4)   #Pembagian (Selalu menghasilkan float)
+#
+i = 51
+j = 6
+hasil5 = i%j
+print(i,'%',j,'=',hasil5)   #Sisa dari hasil pembagian (ex : 15 / 6 = 2 sisa 3, maka % = 3)
+#
+k = 19
+l = 77
+hasil6 = k**l
+print(k,'**',l,'=',hasil6)  #Pemangkatan (Akar dapat dicari menggunakan pemangkatan, ex: √4 = 4^1/2)
+#
+m = 23
+n = 7
+hasil7 = m//n
+print(m,'//',n,'=',hasil7)  #Pembulatan dari hasil pembagian (ex : 20 // 6 = 3)
